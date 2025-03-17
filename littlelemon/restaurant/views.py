8fad18983573307eaf21182cfda08e353de56292
mrjoeladeniyi <joeloadeniyi@gmail.com>
2025-03-17 17:06:07 +0100

    build: add authentication to API using djoser

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -15,10 +15,12 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
     
 class SingleMenuItemView(generics.RetrieveUpdateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
     
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
